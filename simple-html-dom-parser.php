59176076cefb9c3e3b9751002c54075c7cd1145c
AYK722 <toyozumi@edomtt.co.jp>
2020-06-25 12:46:45 +0900

    スクレイピングプログラム修正

--- simple-html-dom-parser.php
+++ simple-html-dom-parser.php
@@ -1,50 +1,54 @@
 <?php
-
 require_once("simple_html_dom.php");
+require_once( dirname(dirname(dirname(dirname( __FILE__ )))) . '/wp-load.php' );
 
+// 手作業コーナー
+$post_id = 1204; //投稿する記事のID
+$prefectures_name = '福島県'; //スラッグ名
+$prefectures_id = 'fukushima'; //スラッグID
+$table_num = 6; // スクレイピングする対象のテーブルナンバー
+$row_num = 7; // カスタムフィールドに追加する行ナンバー
+
+
+// shoplistフィールドを新しく追加
+$parent_row = array(
+    // フィールド名：repeat_prefectures フィールドキー：'field_5eec289fce02b'
+    'field_5eec289fce02b' => array(
+        // フィールド名：prefectures_name フィールドキー：'field_5eec28f5ce02c'
+        'field_5eec28f5ce02c' => $prefectures_name,
+        // フィールド名：prefectures_name_id フィールドキー：'field_5eec2925ce02d'
+        'field_5eec2925ce02d' => $prefectures_id,
+    )
+);
+add_row('field_5eec2845ce029', $parent_row, $post_id);
+
+// simple-html-dom-parserで対象ページスクレイピング
 $url = 'http://fujikurashaft.test.com/shoplist/hokkaido_tohoku/';
-
 $html = file_get_html($url);
 
+// shoplistフィールド内の各サブフィールドに値を追加
+foreach ($html->find('table', $table_num)->find('tr') as $tr) {
+    $shop_name = $tr->find("td", 0)->plaintext;
+    $shop_add = $tr->find("td", 1)->plaintext;
+    $shop_tel = $tr->find("td", 2)->plaintext;
+    $shop_url = $tr->find("a", 1)->href;
+    $map_url = $tr->find("a", 0)->href;
+    //echo $shop_name."\n";
+    //echo $shop_add."\n";
+    //echo $shop_tel."\n";
+    //echo $shop_url."\n";
+    //echo $map_url."\n";
 
-// 1.この書き方だとtdタグ内のplaintext（ショップ名、住所、電話番号、"webサイトへ"の文字列）が取得できる
-foreach ($html->find('td') as $el) {
-    $shop_name = $el->plaintext;
-    echo $shop_name."\n";
-}
+    $children_row = array(
+        'field_5eec5f4b8be08' => $shop_name,
+        'field_5eec5f608be09' => $shop_add,
+        'field_5eec5f708be0a' => $shop_tel,
+        'field_5eec5f7f8be0b' => $shop_url,
+        'field_5eec5f978be0c' => $map_url
+    );
 
+    add_sub_row( array('field_5eec2845ce029', $row_num, 'field_5eec5f228be07'), $children_row, $post_id);
 
-// 2.ショップ名のみを取得したい(trタグ直下の一番目のtdタグのplaintext)場合、この書き方だとうまく行かない…
-foreach ($html->find("tr") as $el) {
-    foreach($el->find("td", 0) as $e) {
-        $shop_name = $e->plaintext;
-        echo $shop_name."\n";
-    }
-}
-
-
-// 3.aタグのhrefの値(地図のURL)を取得
-foreach ($html->find('td') as $el) {
-    foreach ($el->find('a') as $e) {
-        $map_url = $e->href;
-        echo $map_url."\n";
-    }
-}
-
-
-// 4.class名がshoplist_tab_telのtdタグの値（電話番号）を取得
-foreach ($html->find('td.shoplist_tab_tel') as $el) {   
-    $shop_tel = $el->plaintext;
-    echo $shop_tel."\n";
-}
-
-
-// 5.class名がshoplist_tab_homeのtdタグ内のaタグのhrefの値（WEBサイトURL）を取得
-foreach ($html->find('td.shoplist_tab_home') as $el) {
-    foreach ($el->find("a") as $e) {
-        $shop_url = $e->href;
-        echo $shop_url."\n";
-    }
 }
 
 ?>
